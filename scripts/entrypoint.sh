b933d27b648177a1f34806ffb63ffd0f36670af9
testdasi <51292604+testdasi@users.noreply.github.com>
2021-04-13 20:16:08 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-echo '[info] Not much to do...'
+echo '[info] Start glusterd service'
+echo ''
+systemctl start glusterd
+
+echo '[info] Not much else to do...'
 sleep_time=3600
 echo ''
 while true
 do
+    gluster peer status
+    gluster volume status
     sleep $sleep_time
 done
