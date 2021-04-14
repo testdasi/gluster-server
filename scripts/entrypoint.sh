e002c7542ced6327c96f341da41ce085bdd2a454
testdasi <51292604+testdasi@users.noreply.github.com>
2021-04-14 19:04:12 +0100

    Add section to mount glusterfs volumes

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ -f "/etc/glusterfs/glusterd.vol" ]]; then
-    echo '[info] Glusterd management volume exists. Do nothing.'
+    echo '[info] Glusterd management volume exists. Skip fixing.'
 else
-    echo '[info] Glusterd management volume id missing. Copy over default.'
+    echo '[info] Glusterd management volume id missing. Copy over default to fix.'
     cp -n /glusterd.vol /etc/glusterfs/
 fi
 
@@ -13,6 +13,18 @@ glusterd --log-level ERROR
 echo '[info] Wait 30s for things to settle down.'
 sleep_time=30
 sleep $sleep_time
+echo ''
+
+if [ -z "$MOUNT_PATH" ] || [ -z "$GLUSTER_VOL" ];
+then
+  echo '[info] Mount variables not set (or set incorrectly). Skip mounting.'  
+else
+  echo '[info] Attempting to mount...'  
+  for i in $(echo $GLUSTER_VOL | sed "s/,/ /g")
+  do
+    mount.glusterfs localhost:/$i $MOUNT_PATH/$i
+  done
+fi
 echo ''
 
 echo '[info] Healthcheck is running in background.'
