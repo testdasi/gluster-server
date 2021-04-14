592aee3f08dcb5f3edd4358e47d9a71029f83a93
testdasi <51292604+testdasi@users.noreply.github.com>
2021-04-14 14:31:00 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -13,15 +13,20 @@ glusterd --log-level ERROR
 echo '[info] Wait 30s for things to settle down.'
 sleep_time=30
 sleep $sleep_time
+echo ''
 
 echo '[info] Healthcheck is running in background.'
-
 echo '[info] Not much else to do...'
 sleep_time=3600
 echo ''
+
 while true
 do
+    echo '[info] Quick status:'
+    echo ''
     gluster peer status
+    echo ''
     gluster volume status
+    echo ''
     sleep $sleep_time
 done
