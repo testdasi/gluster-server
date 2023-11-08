30a0fdf21ad81485027c31d1e65b625e9bd12e80
Hackerman <25833050+xnojack@users.noreply.github.com>
2023-11-08 02:01:35 +0000

    need to install fuse

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,10 +2,14 @@
 
 # install more packages
 apt-get -y update \
-    && apt-get -y install attr psmisc glusterfs-server
+    && apt-get -y install attr psmisc glusterfs-server fuse
 
 # Clean up
 apt-get -y autoremove \
     && apt-get -y autoclean \
     && apt-get -y clean \
     && rm -fr /tmp/* /var/tmp/* /var/lib/apt/lists/*
+
+# Fix script permissions
+chmod 770 /entrypoint.sh
+chmod 770 /healthcheck.sh
